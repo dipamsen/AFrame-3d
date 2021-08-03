65448a1596757c90759a2c3887339e61d738fce1
Fun Planet <dipamdiptam@gmail.com>
2021-08-03 19:51:25 +0530

    customcar2w

--- customcar-v2/Car.js
+++ customcar-v2/Car.js
@@ -1,20 +1,14 @@
 AFRAME.registerComponent("car", {
   schema: {
-    counter: { type: "number", default: 0 }
+    counter: { type: "number", default: 0 },
   },
   init() {
-    this.handleClickEvent()
+    this.handleClickEvent();
   },
   handleClickEvent() {
     window.addEventListener("click", () => {
-      console.log("CLICKED")
-      this.data.counter += 1
-    })
+      this.data.counter += 1;
+      this.el.setAttribute("rotation", `0 ${(this.data.counter % 4) * 90} 0`);
+    });
   },
-  tick() {
-    console.log(this.data.counter)
-    const xOff = 30
-    // const { x, y, z } = this.el.getAttribute("position")
-    // this.el.setAttribute("position", { x: x + xOff, y, z })
-  }
 });
