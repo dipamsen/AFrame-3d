21ea3c25e0fffdfa3476bf0bb0107f9d61b5086d
Fun Planet <dipamdiptam@gmail.com>
2021-08-11 19:39:10 +0530

    collision-detection

--- shooter/components/Bullet.js
+++ shooter/components/Bullet.js
@@ -11,7 +11,7 @@ AFRAME.registerComponent("bullets", {
         const camera = document.querySelector("#camera");
         const pos = camera.getAttribute("position");
         bullet.setAttribute("position", { x: pos.x, y: pos.y, z: pos.z });
-        // bullet.setAttribute("velocity", { x: 0, y: 0, z: -0.8 });
+        bullet.setAttribute("dynamic-body", { shape: "sphere", mass: 0 });
 
         const camObj = document.querySelector("#camera").object3D;
         const dir = new THREE.Vector3();
@@ -20,9 +20,28 @@ AFRAME.registerComponent("bullets", {
 
         bullet.setAttribute("velocity", dir.multiplyScalar(-10));
 
+        bullet.addEventListener("collide", this.collideHandler);
+
         const scene = document.querySelector("#scene");
         scene.appendChild(bullet);
       }
     });
+  },
+  collideHandler(e) {
+    const bullet = e.detail.target.el; // el
+    const box = e.detail.body.el; // elHit
+
+    if (box.id.includes("box")) {
+      //      box.setAttribute("material", { opacity: 0.6, transparent: true });
+      bullet.removeEventListener("collide", this.shootBullet);
+
+      const scene = document.querySelector("#scene");
+      scene.removeChild(bullet);
+
+      const impulse = new CANNON.Vec3(-2, 2, 1);
+      const worldPoint = new CANNON.Vec3().copy(box.getAttribute("position"));
+
+      box.body.applyImpulse(impulse, worldPoint);
+    }
   },
 });
